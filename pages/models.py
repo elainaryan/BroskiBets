36f2d94d79ff3e4b9b531ef3af8662a8c5591a4d
elainaryan <elainaryan24@gmail.com>
2021-10-27 23:49:32 -0700

    Live website (first version) * Favicon logo * Logo is now in navbar and footer * Pictures of Scott and Kane added on home page * On about page, days since Broski Bets started updates daily * On contact us page, Instagram and email logos appear on same line on mobile

--- pages/models.py
+++ pages/models.py
@@ -1,5 +1,1 @@
 from django.db import models
-
-# class HomePage(Page):
-#     template = "templates/home.html"
-#     max_count = 1 # no child pages
--- pages/templatetags/pages_tags.py
+++ pages/templatetags/pages_tags.py
@@ -1,4 +1,5 @@
 import datetime
+from datetime import date
 from django import template
 from picks.models import DailyPicks
 
@@ -8,6 +9,11 @@ register = template.Library()
 @register.simple_tag
 def get_todays_picks():
     return DailyPicks.objects.order_by("-pub_date").first()
+
+@register.simple_tag
+def get_todays_pick_date():
+    date_obj = DailyPicks.objects.order_by("-pub_date").first().pub_date
+    return str(date_obj.month) + "/" + str(date_obj.day) + "/" + str(date_obj.year)
 
 @register.simple_tag
 def get_scotts_picks(daily_pick_object):
@@ -27,6 +33,12 @@ def get_kanes_picks(daily_pick_object):
 def determine_larger_table(daily_pick_object):
     if daily_pick_object == None:
         return True
-    if daily_pick_object.kanepick_set.count() > daily_pick_object.scottpick_set.count():
+    if daily_pick_object.scottpick_set.count() > daily_pick_object.kanepick_set.count():
         return False
     return True
+
+@register.simple_tag()
+def days_since_day_1():
+    start_date = date(2021, 3, 6)
+    todays_date = date.today()
+    return (todays_date - start_date).days
